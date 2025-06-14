9e3c299d8f43af5ecc9beeb46392042e94a7effc
Thehotday <71886213+66622a@users.noreply.github.com>
2025-06-14 09:36:10 +0000

    feat:使用体验优化 - 加入课程表加载动画 - 修复选择日期加载周课表时的错误 - 加入深色模式自适应 - cookie过期时间改为15天 - 修复重复获取空日期周课表的报错

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import ElementPlus from 'element-plus';
 import App from './App.vue';
 import router from './router';
 import 'element-plus/dist/index.css';
+import 'element-plus/theme-chalk/dark/css-vars.css'
 import dayjs from 'dayjs'
 
 const app = createApp(App);
@@ -11,4 +12,17 @@ app.use(createPinia());
 app.use(router);
 app.use(ElementPlus);
 app.config.globalProperties.$dayjs = dayjs
+
+// 自动切换 Element Plus 深色模式
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.documentElement.classList.add('dark')
+}
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+  if (e.matches) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+})
+
 app.mount('#app');
